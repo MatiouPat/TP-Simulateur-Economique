85b69474efa88455c5141ffa78cbf95c75cf70fb
Virgile <virgilevallvillellas@gmail.com>
2023-12-01 13:35:36 +0100

    fix bug

--- TP-Simulateur-Economique/Production.cpp
+++ TP-Simulateur-Economique/Production.cpp
@@ -7,11 +7,10 @@
 #include "Company.hpp"
 #include "Merchandise.hpp"
 
-Production::Production(std::map<Merchandise, int> _incoming, Merchandise _outgoing, Company _company, float _price, unsigned int _dailyProd, std::map<Merchandise, int> _prodCost, unsigned int _employee)
+Production::Production(std::map<Merchandise, int> _incoming, Merchandise _outgoing, float _price, unsigned int _dailyProd, std::map<Merchandise, int> _prodCost, unsigned int _employee)
 {
 	incoming = _incoming;
 	outgoing = _outgoing;
-	company = _company;
 	price = _price;
 	dailyProd = _dailyProd;
 	prodCost = _prodCost;
--- TP-Simulateur-Economique/Production.hpp
+++ TP-Simulateur-Economique/Production.hpp
@@ -9,14 +9,13 @@ class Production
 public:
 
 	//Production();
-	Production(std::map<Merchandise, int> _incoming, Merchandise _outgoing, Company _company, float _price, unsigned int _dailyProd, std::map<Merchandise,int> _prodCost, unsigned int _employee);
+	Production(std::map<Merchandise, int> _incoming, Merchandise _outgoing, float _price, unsigned int _dailyProd, std::map<Merchandise,int> _prodCost, unsigned int _employee);
 	~Production();
 
 protected:
 
 	std::map<Merchandise, int> incoming;
 	Merchandise outgoing;
-	Company company;
 	float price;
 	unsigned int dailyProd;
 	std::map<Merchandise, int> prodCost;
