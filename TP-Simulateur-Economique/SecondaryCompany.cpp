9e6b5e43d6dce1ef42de9b964c5cdfdc8d102341
TomL <63425616+Tomeau@users.noreply.github.com>
2023-12-01 09:06:13 +0100

    Update SecondaryCompany.cpp

--- TP-Simulateur-Economique/SecondaryCompany.cpp
+++ TP-Simulateur-Economique/SecondaryCompany.cpp
@@ -9,8 +9,29 @@ SecondaryCompany::SecondaryCompany(std::vector<Merchandise> _entrant, std::map<M
 /**
 *
 **/
-void SecondaryCompany::transform()
+void SecondaryCompany::transform(Company* pCompany)
 {
+	for (int i = 0, i <= size(pCompany->entrant), i++)
+	{
+		if (pCompany->stock[pCompany->entrant[i]] - pCompany->coutProd[pCompany->entrant[i]] >= 0) {
+			if (pCompany->stock[pCompany->sortant] + 1 <= pCompany->stockMax[pCompany->sortant])
+			{
+				for (int i = 0, i <= size(pCompany->entrant), i++)
+				{
+					pCompany->stock[pCompany->entrant[i]] -= pCompany->coutProd[pCompany->entrant[i]];
+				}
+				pCompany->stock[pCompany->sortant] += 1;
+			}
+			else
+			{
+				std::cerr << "Outgoing products storage full. " << std::endl;
+			}
+		}
+		else
+		{
+			std::cerr << "Empty storage for incoming products. " << std::endl;
+		}
+	}	
 }
 
 
