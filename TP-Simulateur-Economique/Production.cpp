209ad1b26531083e2fdfb1dcf13e8306a3fa5c87
TomL <63425616+Tomeau@users.noreply.github.com>
2023-12-01 11:19:36 +0100

    Create Production.cpp

---
+++ TP-Simulateur-Economique/Production.cpp
@@ -0,0 +1,23 @@
+// Company.hpp
+
+#include "Production.hpp"
+#include <string>
+#include <vector>
+#include <map>
+#include "Company.hpp"
+#include "Merchandise.hpp"
+
+Production::Production(std::map<Merchandise, int> _incoming, Merchandise _outgoing, Company _company, float _price, unsigned int _dailyProd, std::map<Merchandise, int> _prodCost, unsigned int _employee)
+{
+	incoming = _incoming;
+	outgoing = _outgoing;
+	company = _company;
+	price = _price;
+	dailyProd = _dailyProd;
+	prodCost = _prodCost;
+	employee = _employee;
+}
+
+Production::~Production()
+{
+}
