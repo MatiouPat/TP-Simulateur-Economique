999fd5f0f82f52bf1e143286648188180e8ef1c9
Matthieu PAYS <pays.matthieuic@gmail.com>
2023-12-01 14:30:38 +0100

    Revert "Update SecondaryCompany.hpp"
    This reverts commit 8f53b55a55dc446f2f1d1ae824f76aa8e40d8aa0.

--- TP-Simulateur-Economique/SecondaryCompany.hpp
+++ TP-Simulateur-Economique/SecondaryCompany.hpp
@@ -3,7 +3,6 @@
 
 #include "Company.hpp"
 #include "Transformation.hpp"
-#include "Production.hpp"
 
 class SecondaryCompany : public Company
 {
@@ -11,11 +10,11 @@ public:
 
 	SecondaryCompany();
 	SecondaryCompany(std::string _name, unsigned int _level, double _capital,
-		std::vector<Employee> _listEmployee, std::map<Merchandise, int> _stock, std::map<Merchandise, int> _stockMax, std::shared_ptr<Production> _production);
+		std::vector<Employee> _listEmployee, std::map<Merchandise, int> _stock, std::map<Merchandise, int> _stockMax);
 	~SecondaryCompany();
-	std::string getName();
+	std::string getName() const override;
 	void addEmployee(Employee _employee);
-	void transform();
+	void transform(Company* pCompany);
 	void buy();
 	template <typename CompanyType>
 	Company findSupplier(Transformation<CompanyType> transfo);
