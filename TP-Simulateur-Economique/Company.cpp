1f66cf22ecba20afe42d56118580b00e31501e03
Matthieu PAYS <pays.matthieuic@gmail.com>
2023-12-01 14:30:38 +0100

    Revert "Update Company.cpp"
    This reverts commit 1d26f4d247d6a4ca0333cc9fb18e1dd5175b2041.

--- TP-Simulateur-Economique/Company.cpp
+++ TP-Simulateur-Economique/Company.cpp
@@ -6,6 +6,7 @@
 #include <map>
 #include "Employee.hpp"
 #include "Merchandise.hpp"
+#include "Production.hpp"
 
 
 Company::Company()
@@ -17,7 +18,7 @@ Company::Company()
     stock = std::map<Merchandise, int>();
 }
 
-Company::Company(std::string _name, unsigned int _level, double _capital, std::vector<Employee> _listEmployee, std::map<Merchandise, int> _stock, std::map<Merchandise, int> _stockMax, std::shared_ptr<Production> _production)
+Company::Company(std::string _name, unsigned int _level, double _capital, std::vector<Employee> _listEmployee, std::map<Merchandise, int> _stock, std::map<Merchandise, int> _stockMax)
 {
     name = _name;
     level = _level;
@@ -25,9 +26,31 @@ Company::Company(std::string _name, unsigned int _level, double _capital, std::v
     listEmployee = _listEmployee;
     stock = _stock;
     stockMax = _stockMax;
-    prod = _production;
 }
 
 Company::~Company()
 {
+}
+
+void Company::addEmployee(Employee _employee)
+{
+}
+
+void Company::levelUp()
+{
+    level++;
+}
+
+void Company::print()
+{
+    std::cout << name << std::endl;
+    std::cout << "\tNiveau : " << level << std::endl;
+    std::cout << "\tCapital : " << capital << std::endl;
+    std::cout << "\tStocke : " << std::endl;
+    for (auto& s : stock) {
+        Merchandise m(s.first);
+        std::cout << "\t\tMarchandise : " << m.getName() << ", Nombre : " << s.second << std::endl;
+    }
+    std::cout << std::endl;
+
 }
