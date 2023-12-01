1e61a8011099694fe25b2835e1d53116ef0b64a4
TomL <63425616+Tomeau@users.noreply.github.com>
2023-12-01 14:17:44 +0100

    Update Production.hpp

--- TP-Simulateur-Economique/Production.hpp
+++ TP-Simulateur-Economique/Production.hpp
@@ -4,18 +4,24 @@
 #include "Merchandise.hpp" 
 #include "Company.hpp"
 
+class Company;
+
 class Production
 {
 public:
 
 	//Production();
-	Production(std::map<Merchandise, int> _incoming, Merchandise _outgoing, float _price, unsigned int _dailyProd, std::map<Merchandise,int> _prodCost, unsigned int _employee);
+	Production(std::map<Merchandise, int> _incoming, Merchandise _outgoing, std::shared_ptr<Company> _company, float _price, unsigned int _dailyProd, std::map<Merchandise,int> _prodCost, unsigned int _employee);
 	~Production();
+	std::map<Merchandise, int> getIncoming();
+	/*Merchandise getIncomingByID(int n);*/
+	std::map<Merchandise, int> getProdCost();
 
 protected:
 
 	std::map<Merchandise, int> incoming;
 	Merchandise outgoing;
+	std::shared_ptr<Company> company;
 	float price;
 	unsigned int dailyProd;
 	std::map<Merchandise, int> prodCost;
