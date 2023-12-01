6074e6523875fa88a8331f600a44b4d173fd7913
Matthieu PAYS <pays.matthieuic@gmail.com>
2023-12-01 14:30:38 +0100

    Revert "Update Production.cpp"
    This reverts commit a26f2e896fccd300301ddeeb8f5b8a28c49b3e5c.

--- TP-Simulateur-Economique/Production.cpp
+++ TP-Simulateur-Economique/Production.cpp
@@ -7,11 +7,10 @@
 #include "Company.hpp"
 #include "Merchandise.hpp"
 
-Production::Production(std::map<Merchandise, int> _incoming, Merchandise _outgoing, std::shared_ptr<Company> _company, float _price, unsigned int _dailyProd, std::map<Merchandise, int> _prodCost, unsigned int _employee)
+Production::Production(std::map<Merchandise, int> _incoming, Merchandise _outgoing, float _price, unsigned int _dailyProd, std::map<Merchandise, int> _prodCost, unsigned int _employee)
 {
 	incoming = _incoming;
 	outgoing = _outgoing;
-	company = _company;
 	price = _price;
 	dailyProd = _dailyProd;
 	prodCost = _prodCost;
@@ -21,22 +20,3 @@ Production::Production(std::map<Merchandise, int> _incoming, Merchandise _outgoi
 Production::~Production()
 {
 }
-
-std::map<Merchandise, int> Production::getIncoming()
-{
-	return incoming;
-}
-
-
-//Merchandise Production::getIncomingByID(int n)
-//{
-//	std::pair<Merchandise, int> retour = incoming.begin();
-//	std::advance(retour, n);
-//	return retour.first;
-//}
-
-std::map<Merchandise, int> Production::getProdCost()
-{
-	return prodCost;
-}
-
