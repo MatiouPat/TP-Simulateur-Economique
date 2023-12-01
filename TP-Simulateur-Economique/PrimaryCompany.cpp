663ee72ee83ddcbad4e38287aa7a7db63506a80f
TomL <63425616+Tomeau@users.noreply.github.com>
2023-12-01 09:07:38 +0100

    Update PrimaryCompany.cpp

--- TP-Simulateur-Economique/PrimaryCompany.cpp
+++ TP-Simulateur-Economique/PrimaryCompany.cpp
@@ -1,5 +1,13 @@
 #include "PrimaryCompany.hpp"
 
-std::vector<Merchandise> PrimaryCompany::create()
+void PrimaryCompany::create(Company* _company)
 {
+	if (pCompany->stock[pCompany->sortant] + 1 <= pCompany->stockMax[pCompany->sortant])
+	{
+		pCompany->stock[pCompany->sortant] += 1;
+	}
+	else
+	{
+		std::cerr << "Outgoing products storage full. " << std::endl;
+	}
 }
