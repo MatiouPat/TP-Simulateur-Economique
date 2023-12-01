ef9568ee41bfd36fd8c2a9b2c56d0e70fa7c86c5
TomL <63425616+Tomeau@users.noreply.github.com>
2023-12-01 14:17:21 +0100

    Update PrimaryCompany.hpp

--- TP-Simulateur-Economique/PrimaryCompany.hpp
+++ TP-Simulateur-Economique/PrimaryCompany.hpp
@@ -9,10 +9,11 @@ public:
 
 	PrimaryCompany();
 	PrimaryCompany(std::string _name, unsigned int _level, double _capital,
-		std::vector<Employee> _listEmployee, std::map<Merchandise, int> _stock, std::map<Merchandise, int> _stockMax);
+		std::vector<Employee> _listEmployee, std::map<Merchandise, int> _stock, std::map<Merchandise, int> _stockMax, std::shared_ptr<Production> _production);
 	~PrimaryCompany();
-	std::string getName() const override;
+	std::string getName();
 	void addEmployee(Employee _employee);
+
 	void create();
 
 private:
